2e494d0af70c70054bc850632fd3b21b7b361733
Ryan Stock <contact@ryanstock.com.au>
2015-03-29 02:22:56 +1100

    Initial commit

---
+++ jquery.echoable.js
@@ -0,0 +1,131 @@
+/*
+
+Echoable jQuery Plugin by Ryan Stock
+
+www.github.com/ryanstockau/echoable
+
+www.ryanstock.com.au
+
+*/
+
+ 
+;(function ( $, window, document, undefined ) {
+	 
+	// Plugin Constructor
+	
+  	var Echoable = function( elem, options ){
+		var self = this;
+		self.elem = elem;
+		self.$elem = $(elem);
+		self.options = options;
+		
+		// Allow options to be presented via HTML data-plugin-options attribute, eg:
+		// <div class='item' data-plugin-options='{"message":"Goodbye World!"}'></div>
+		self.metadata = self.$elem.data( 'plugin-options' );
+    };
+	
+	
+	// Plugin Prototype
+	
+	Echoable.prototype = {
+		
+		defaults: {
+			echoableClass : 'echoable',
+			clickable : true,
+			clickTarget : 'click touchend',
+			largeWidth : 400,
+			triggerDelay : 40,
+			animimationDelay : 1000,
+			largeClass : 'echoable-large'
+		},
+		
+		init: function() {
+			
+			var self = this;
+			
+			// Add a data reference to this object
+			self.$elem.data('echoable', self);
+			
+			// Set combined config
+			self.config = $.extend({}, self.defaults, self.options, self.metadata);
+		
+			var $echos = $('<span class="echos" />');
+			
+			if ( self.$elem.is('span, a') ) {
+				self.$elem.wrapInner( '<span class="echoable-content" />' );
+			} else {
+				self.$elem.wrapInner( '<div class="echoable-content" />' );
+			}
+			
+			var $echo = $('<span class="echo" />');
+			if ( self.$elem.is('.echoable') ) {
+			//		$echo.clone().addClass('echo-main').appendTo($echos); TODO
+			}
+			if ( self.$elem.is('.btn-animate-hover') ) {
+				self.$hover_echo = $echo.clone().addClass('echo-hover').appendTo($echos);
+			}
+			if ( self.$elem.is('.btn-animate-click, .echo-on-click') ) {
+				self.$click_echo = $echo.clone().addClass('echo-click').appendTo($echos);
+			}
+			
+			self.$elem.prepend( $echos );	
+			
+			self._initHandlers();
+			
+			return self;
+		},
+		
+		triggerEcho : function() {
+			var self = this;
+			var $el = self.$elem;
+			var x_offset = e.pageX - $el.offset().left;
+			var y_offset = e.pageY - $el.offset().top;
+			var xPercent = x_offset / $el.outerWidth() * 100;
+			var yPercent = y_offset / $el.outerHeight() * 100;
+			var $click_echo = self.$click_echo;
+			
+			$click_echo
+				.css('left', xPercent + '%' )
+				.css('top', yPercent + '%' ); 
+		
+			if ( $el.width() > self.config.largeWidth ) {
+				$el.addClass( self.config.largeClass );
+			}
+		
+			setTimeout( function() {
+				$el.addClass('clicking echoing');
+				setTimeout(function(){
+					$el.removeClass('clicking echoing echoable-large');
+				}, self.config.animationDelay);
+			}, self.config.triggerDelay );
+		},
+		
+		_initHandlers : function() {
+			var self = this;
+			if ( self.config.clickable ) {
+				self.$elem.on(self.config.clickTarget, self._handleClick);
+			}
+		},
+		
+		_handleClick : function( e ) {
+			var self = this;
+			e.preventDefault();
+			console.log( this );
+			self.triggerEcho();
+			return false;			
+		}
+		
+	}
+	
+	Echoable.defaults = Echoable.prototype.defaults;
+	
+	
+	// Create plugin
+	
+	$.fn.echoable = function(options) {
+		return this.each(function() {
+			new Echoable(this, options).init();
+		});
+	};
+	
+})( jQuery, window , document );
